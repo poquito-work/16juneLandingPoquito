da7b058beebb7da54c09d4c11b7ecfa992f89076
Prachi <prachi.s@weqtechnologies.com>
2026-06-29 19:55:06 +0530

    client feedbacks final

--- src/routes/dashboard.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute, redirect } from "@tanstack/react-router";
-
-export const Route = createFileRoute("/dashboard")({
-  head: () => ({ meta: [{ title: "My Account — Pocket Dragon" }] }),
-  // beforeLoad: () => {
-  //   throw redirect({ to: "/myaccount/profile" });
-  // },
-  component: () => null,
-});
