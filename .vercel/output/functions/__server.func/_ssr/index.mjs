b1594efcf0d29a01305358d1d66336930f4ab762
Prachi <prachi.s@weqtechnologies.com>
2026-07-28 17:05:03 +0530

    dynamic package

--- .vercel/output/functions/__server.func/_ssr/index.mjs
+++ .vercel/output/functions/__server.func/_ssr/index.mjs
@@ -53,7 +53,7 @@ function renderErrorPage() {
 let serverEntryPromise;
 async function getServerEntry() {
   if (!serverEntryPromise) {
-    serverEntryPromise = import("./server-U9ffvui7.mjs").then((n) => n.s).then(
+    serverEntryPromise = import("./server-DbjnjjFV.mjs").then((n) => n.s).then(
       (m) => m.default ?? m
     );
   }
