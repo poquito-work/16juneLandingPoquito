3f4299633a7c49f28298729a262bd23ebf5f9aa7
Prachi <prachi.s@weqtechnologies.com>
2026-09-03 22:12:05 +0530

    chnage api url

--- src/components/poquito/DashboardPage.tsx
+++ src/components/poquito/DashboardPage.tsx
@@ -1419,6 +1419,7 @@ function SubscriptionTab({
           </div> */}
         </div>
       )}
+      
 
       {!trialEndedWithoutSubscription && !subscription && isTrialActive && (
         <div className="dash-sub-current">
